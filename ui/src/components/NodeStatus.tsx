8e42e884c8e8ecabbedff3a1ee750fb6b0cdc0fa
itsnoproblem <noproblem@mulligan.xyz>
2022-02-03 21:16:38 -0500

    clean up imports

--- ui/src/components/NodeStatus.tsx
+++ ui/src/components/NodeStatus.tsx
@@ -1,25 +1,5 @@
-import {
-    Box,
-    Button,
-    HStack, IconButton,
-    Popover,
-    PopoverBody,
-    PopoverContent,
-    PopoverTrigger,
-    Spacer,
-    Text,
-    useBreakpointValue
-} from "@chakra-ui/react";
-import {AiFillStar, AiOutlineStar, MdBrightness1} from "react-icons/all";
 import * as React from "react";
-import {useCallback, useEffect, useState} from "react";
-import axios from "axios";
-import {CryptoNode, NodeProps} from "../types/crypto-node";
-import {useParams} from "react-router-dom";
-import {StarIcon} from "@chakra-ui/icons";
-import {useLocalStorage} from "react-use";
-import {NodeChooser} from "./NodeChooser";
-
+import {NodeProps} from "../types/crypto-node";
 
 
 export const NodeStatus = (props: NodeProps) => {
