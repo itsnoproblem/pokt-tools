d698025d98d00cb8361da461a98f3e392b662651
itsnoproblem <noproblem@mulligan.xyz>
2022-02-09 20:13:32 -0500

    cleanup comments

--- ui/src/components/DayOfWeekChart.tsx
+++ ui/src/components/DayOfWeekChart.tsx
@@ -9,8 +9,8 @@ type DayOfWeekChartProps = {
 
 export const DayOfWeekChart = (props: DayOfWeekChartProps) => {
     const margin = useBreakpointValue([{ top: 0, right: 80, bottom: 80, left: 80 }, {top: 40, right:180, bottom:80, left: 80}]);
-    const isMobile = useBreakpointValue([true, false]);
-    const legendItemWidth = useBreakpointValue([70, 90]) ?? 90
+    // const isMobile = useBreakpointValue([true, false]);
+    // const legendItemWidth = useBreakpointValue([70, 90]) ?? 90
 
     console.log("bar", props.data);
 
@@ -22,35 +22,6 @@ export const DayOfWeekChart = (props: DayOfWeekChartProps) => {
                 margin={margin}
                 borderWidth={1}
                 borderColor={'#ccc'}
-
-                // innerRadius={0.5}
-                // padAngle={0.7}
-                // cornerRadius={3}
-                // activeOuterRadiusOffset={8}
-
-                // arcLinkLabelsSkipAngle={10}
-                // arcLinkLabelsTextColor="#ccc"
-                // arcLinkLabelsThickness={2}
-                // arcLinkLabelsColor={"#ccc"}
-                // arcLabelsSkipAngle={10}
-                // arcLabelsTextColor={"#000"}
-                // legends={[
-                //     {
-                //         anchor: isMobile ? 'bottom' : 'bottom',
-                //         direction: 'row',
-                //         justify: false,
-                //         translateX: 0,
-                //         translateY: 56,
-                //         itemsSpacing: 4,
-                //         itemWidth: legendItemWidth,
-                //         itemHeight: 18,
-                //         itemTextColor: '#ccc',
-                //         itemDirection: 'left-to-right',
-                //         itemOpacity: 1,
-                //         symbolSize: 18,
-                //         symbolShape: 'circle',
-                //     }
-                // ]}
             />
         </Box>
     )
