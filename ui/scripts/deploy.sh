cb5d2f321904f906be0b0bb29cec01c95f47097c
itsnoproblem <noproblem@mulligan.xyz>
2022-04-06 22:43:07 -0400

    add deploy by env

--- ui/scripts/deploy.sh
+++ ui/scripts/deploy.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 BRANCH=$( git rev-parse --abbrev-ref HEAD )
 
-if [ "$BRANCH" != "master" ]; then
+if [ "$1" == "" ]; then
+  echo "** Usage: ${0} <s3-bucket>"
+  exit 1
+fi
+S3_BUCKET=$1
+
+if [ "$BRANCH" != "master" ] && [ "$S3_BUCKET" == "pokt.tools" ]; then
   echo "ERROR: Can only deploy master branch"
   exit 1
 fi
-
+ 
 # copy files to s3
 docker run --rm -it \
   -v ~/.aws:/root/.aws \
   -v ~/dev/projects/itsnoproblem/pokt-calculator/ui/build:/root/build \
-  amazon/aws-cli s3 sync --delete /root/build/ s3://pokt.tools/
+  amazon/aws-cli s3 sync --delete /root/build/ s3://$S3_BUCKET/
 
 # invalidate all objects
 # TODO: optimize this
