1782c2bb338c4c2a453b8f079d8957c768a6744d
itsnoproblem <noproblem@mulligan.xyz>
2022-05-12 04:36:18 -0400

    treat cached params with null values as not found, so they will be re-fetched

--- monitoring-service/pocket/params.go
+++ monitoring-service/pocket/params.go
@@ -1,5 +1,7 @@
 package pocket
 
+import "fmt"
+
 type Params struct {
 	RelaysToTokensMultiplier float64
 	DaoAllocation            uint8
@@ -13,6 +15,30 @@ type AllParams struct {
 	GovParams    ParamGroup `json:"gov_params"`
 	NodeParams   ParamGroup `json:"node_params"`
 	PocketParams ParamGroup `json:"pocket_params"`
+}
+
+func (a AllParams) Validate() error {
+	if a.AppParams == nil {
+		return fmt.Errorf("app_params is null")
+	}
+
+	if a.AuthParams == nil {
+		return fmt.Errorf("auth_params is null")
+	}
+
+	if a.GovParams == nil {
+		return fmt.Errorf("gov_params is null")
+	}
+
+	if a.NodeParams == nil {
+		return fmt.Errorf("node_params is null")
+	}
+
+	if a.PocketParams == nil {
+		return fmt.Errorf("pocket_params is null")
+	}
+
+	return nil
 }
 
 type ParamGroup []Param
